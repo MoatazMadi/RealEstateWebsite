b961f4ce5e94c4c7d39a5da490dcf66baa250e97
moatazMadi <moataz_madi@outlook.com>
2025-03-10 02:23:16 +0200

    Updates

--- src/components/navBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <main class="h-[85vh] relative bg-cover bg-center" style="background-image: url('/imgs/hero.jpg');">
-      
-  </main>
-  
-  </template>
-  
-  <script setup></script>
-  
